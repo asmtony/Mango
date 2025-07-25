c84456508a4d2324261cc0961820a65df2e86f92
Anthony McEvoy <asmtony@hotmail.co.uk>
2025-07-25 11:01:13 +0100

    Added login and register controller methods

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Web.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Login()
         {
+            LoginRequestDto loginRequestDto = new LoginRequestDto
+            {
+                Username = string.Empty,
+                Password = string.Empty
+            };
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegistrationRequestDto loginRequestDto = new RegistrationRequestDto
+            {
+                Email = string.Empty,
+                Password = string.Empty,
+                FirstName = string.Empty,
+                LastName = string.Empty,
+            };
+            return View(loginRequestDto);
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+
             return View();
         }
     }
